6519bb2ad60a972a22af92adb129ce13c41f4da6
Cristóbal Sarmiento Rodríguez <cristobal.sarmiento101@alu.ulpgc.es>
2024-01-20 21:13:43 +0000

    Class MainFrame created.

---
+++ src/main/java/software/ulpgc/kata7/MainFrame.java
@@ -0,0 +1,17 @@
+package software.ulpgc.kata7;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() throws HeadlessException {
+
+        this.setTitle("Hello Wordl");
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    }
+
+}
